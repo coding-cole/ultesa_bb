0f42d2a3bc9bd8c6d7f985f746faa587189cbae2
jeanc <jeancolexxx@gmail.com>
2022-05-22 21:36:40 +0100

    Added commenting and liking post validations

--- app/routes/comment_and_like.py
+++ app/routes/comment_and_like.py
@@ -19,19 +19,21 @@ async def comment_on_post(
         comment: str = Form(...),
         current_user: dict = Depends(get_current_user),
 ):
-    update_result = await db[POSTS].update_one(
-        {"_id": ObjectId(post_id)}, {
-            "$push": {
-                "comments": {
-                    "comment_owner_id": current_user["_id"],
-                    "comment": comment
+    if (post := await db[POSTS].find_one({"_id": ObjectId(post_id)})) is not None:
+        update_result = await db[POSTS].update_one(
+            {"_id": ObjectId(post_id)}, {
+                "$push": {
+                    "comments": {
+                        "comment_owner_id": current_user["_id"],
+                        "comment": comment
+                    }
                 }
             }
-        }
-    )
-    if update_result.modified_count == 1:
-        return "Thanks for the comment"
-    raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Unable to make comment")
+        )
+        if update_result.modified_count == 1:
+            return "Thanks for the comment"
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Unable to make comment")
+    raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Couldn't find post")
 
 
 @comment_and_like_router.post(
@@ -42,13 +44,17 @@ async def like_post(
         post_id: str,
         current_user: dict = Depends(get_current_user),
 ):
-    update_result = await db[POSTS].update_one(
-        {"_id": ObjectId(post_id)}, {
-            "$push": {
-                "likes": current_user["_id"]
-            }
-        }
-    )
-    if update_result.modified_count == 1:
-        return "Thanks for the like"
-    raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Unable to like this post")
+    if (post := await db[POSTS].find_one({"_id": ObjectId(post_id)})) is not None:
+        if current_user["_id"] not in post["likes"]:
+            update_result = await db[POSTS].update_one(
+                {"_id": ObjectId(post_id)}, {
+                    "$push": {
+                        "likes": current_user["_id"]
+                    }
+                }
+            )
+            if update_result.modified_count == 1:
+                return "Thanks for the like"
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Unable to like this post")
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Already liked this post")
+    raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Couldn't find post")
